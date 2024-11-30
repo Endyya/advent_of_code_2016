5bf54a46ff49eae83c18486215608f4cf0d974b0
Endyya <endya@protonmail.com>
2024-11-30 19:34:54 +0100

    Day 5 * Finished

---
+++ day_5/main.py
@@ -0,0 +1,15 @@
+import hashlib as hl
+
+code1 = 'abbhdwsy'
+
+password = ''
+counter = 0
+while len(password) < 8:
+    to_hash = code + str(counter)
+    hashing = hl.md5(to_hash.encode()).hexdigest()
+    counter += 1
+    if hashing[:5] == '00000':
+        password += hashing[5]
+        
+
+print('part 1 :', password)
