1332a3330abf88ef88d795fc14c34e01cfd09e12
Endyya <endya@protonmail.com>
2024-11-30 11:36:38 +0100

    Day 1 ** finished

--- day_1/main.py
+++ day_1/main.py
@@ -1,6 +1,8 @@
-def find_distance(line):
+
+def find_distance(line, part = 1):
     current_dir = 'N'
     current_coord = [0, 0]
+    traj = [current_coord[::]]
     for inst in line.split(', '):
         dir_inst = inst[0]
         step_number = int(inst[1:])
@@ -8,21 +10,45 @@ def find_distance(line):
         if ((current_dir == 'N' and dir_inst == 'L')
             or (current_dir == 'S' and dir_inst == 'R')):
             current_dir = 'W'
-            current_coord[0] -= step_number
+            for _ in range(step_number):
+                current_coord[0] -= 1
+                if current_coord in traj and part == 2:
+                    return abs(current_coord[0]) + abs(current_coord[1])
+                traj.append(current_coord[::])
+
         elif ((current_dir == 'N' and dir_inst == 'R')
             or (current_dir == 'S' and dir_inst == 'L')):
             current_dir = 'E'
-            current_coord[0] += step_number
+            for _ in range(step_number):
+                current_coord[0] += 1
+                if current_coord in traj and part == 2:
+                    return abs(current_coord[0]) + abs(current_coord[1])
+                traj.append(current_coord[::])
+
         elif ((current_dir == 'W' and dir_inst == 'R')
             or (current_dir == 'E' and dir_inst == 'L')):
             current_dir = 'N'
-            current_coord[1] += step_number
+            for _ in range(step_number):
+                current_coord[1] += 1
+                if current_coord in traj and part == 2:
+                    return abs(current_coord[0]) + abs(current_coord[1])
+                traj.append(current_coord[::])            
+
         elif ((current_dir == 'W' and dir_inst == 'L')
             or (current_dir == 'E' and dir_inst == 'R')):
             current_dir = 'S'
-            current_coord[1] -= step_number            
+            for _ in range(step_number):
+                current_coord[1] -= 1
+                if current_coord in traj and part == 2:
+                    return abs(current_coord[0]) + abs(current_coord[1])
+                traj.append(current_coord[::])
 
+
+
+        
     return abs(current_coord[0]) + abs(current_coord[1])
+
+        
 
 
 
@@ -32,4 +58,4 @@ with open("input") as f:
     line = f.readline()
 
 print("part 1 answer :", find_distance(line))
-print("part 2 answer :", 0)
+print("part 2 answer :", find_distance(line, part = 2))
