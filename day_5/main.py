3bef278cd159ca4ff3d5099ec815e0cec4d6be20
Endyya <endya@protonmail.com>
2024-11-30 19:55:50 +0100

    Day 5 ** Finished

--- day_5/main.py
+++ day_5/main.py
@@ -13,3 +13,21 @@ while len(password) < 8:
         
 
 print('part 1 :', password)
+
+password = 8 * ['0']
+possible_pos = ['0', '1', '2', '3', '4', '5', '6', '7']
+counter = 0
+
+while len(possible_pos) > 0:
+    to_hash = code + str(counter)
+    hashing = hl.md5(to_hash.encode()).hexdigest()
+    counter += 1
+
+    if hashing[:5] == '00000':
+        pos = hashing[5]
+        if pos in possible_pos:
+            password[int(pos)] = hashing[6]
+            possible_pos.remove(pos)
+
+print('part 2 :', ''.join(password))
+            
